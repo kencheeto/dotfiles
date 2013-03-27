340b4188319135a7c542ca7580465b9f6c0e0e36
Kenshiro Nakagawa <kencheeto@gmail.com>
2013-03-27 01:05:28 -0700

    a bit nicer

--- install.rb
+++ install.rb
@@ -1,14 +1,20 @@
-def source filename
-  File.expand_path File.join(__FILE__, '../', filename)
+def source dotfile
+  File.expand_path File.join(__FILE__, '../', dotfile)
+end
+
+def target dotfile
+  File.expand_path File.join(ENV['HOME'], '.' + dotfile)
 end
 
 dotfiles = %w[
-  .bash_profile
-  .gemrc
-  .gitconfig
-  .ruby-version
-  .zendeskrc
+  bash_profile
+  gemrc
+  gitconfig
+  gitignore
+  ruby-version
+  zendeskrc
 ].each do |dotfile|
-  puts "copying #{source(dotfile)} to ~/#{dotfile}"
-  puts system('ln', '-s', source(dotfile), "#{ENV['HOME'] + '/' + dotfile}")
+  command = "ln -s #{source(dotfile)} #{target(dotfile)}"
+  puts command
+  system *command.split(' ')
 end
