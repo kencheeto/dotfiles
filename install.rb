41e7406543c5bc9902626a9847c9e49f9a7f5902
Kenshiro Nakagawa <kencheeto@gmail.com>
2013-05-25 11:29:42 -0700

    symlinking

--- install.rb
+++ install.rb
@@ -1,9 +1,9 @@
-def source dotfile
-  File.expand_path File.join(__FILE__, '../', dotfile)
-end
-
-def target dotfile
-  File.expand_path File.join(ENV['HOME'], '.' + dotfile)
+def symlink source, destination
+  if File.exists?(destination) || File.symlink?(destination)
+    puts destination + ' already exists'
+  else
+    File.symlink(source, destination)
+  end
 end
 
 %w[
@@ -15,7 +15,34 @@ end
   zendesk
   git-prompt.sh
 ].each do |dotfile|
-  command = "ln -s #{source(dotfile)} #{target(dotfile)}"
-  puts command
-  system *command.split(' ')
+  symlink(
+    File.expand_path(File.join(__FILE__, '..', dotfile)),
+    File.expand_path(File.join(ENV['HOME'], '.' + dotfile))
+  )
 end
+
+sublime_root = File.join(
+  File::SEPARATOR,
+  'Users',
+  'kencheeto',
+  'Library',
+  'Application Support',
+  'Sublime Text '
+)
+
+[
+  'Installed Packages',
+  'Packages',
+  'Pristine Packages',
+  'Settings'
+].each do |subdirectory|
+  symlink(
+    File.expand_path(File.join(__FILE__, '..', '..', 'config', 'SublimeText2', subdirectory)),
+    File.expand_path(File.join(sublime_root + '2', subdirectory))
+  )
+end
+
+symlink(
+  File.expand_path(File.join(__FILE__, '..', '..', 'config', 'SublimeText3')),
+  File.expand_path(sublime_root + '3')
+)
