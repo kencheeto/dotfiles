e6dfd87670df0d6caf002cb79f4228e9b6d3b516
Kenshiro Nakagawa <kencheeto@gmail.com>
2015-04-19 22:29:45 -0700

    add cask.sh

---
+++ cask.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# install from app store manually:
+# xcode
+# evernote
+# wunderlist
+# pixelmator
+# pocket
+
+echo installing homebrew
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+
+echo installing homebrew cask
+brew install caskroom/cask/brew-cask
+
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+
+echo tapping caskroom/versions
+brew tap caskroom/versions
+
+echo install utilities
+brew cask install alfred
+brew cask install istat-menus
+brew cask install karabiner
+brew cask install duet
+brew cask install cloak
+brew cask install sizeup
+brew cask install spideroak
+
+echo install core
+brew cask install firefox
+brew cask install chrome
+brew cask install vlc
+brew cask install spotify
+brew cask install slack
+brew cask install mailbox
+brew cask install steam
+
+echo install work
+brew cask install iterm2
+brew cask install heroku-toolbelt
+brew cask install sublime-text3
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install gas-mask
+brew cask install flowdock
+brew cask install licecap
+brew cask install sketch
+
+echo finishing up
+brew cask alfred link
+brew cleanup --force
+rm -f -r /Library/Caches/Homebrew/*
